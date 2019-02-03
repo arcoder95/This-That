4044139ecfba8967781492855da3020fdd834189
Ilmo Kapanen <47299282+arcoder95@users.noreply.github.com>
2019-02-03 18:17:32 +0200

    Add files via upload

---
+++ Arduino_DHT.ino
@@ -0,0 +1,35 @@
+
+
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+#include <dht.h>
+
+#define dht_dpin A0
+
+dht DHT;
+
+
+void setup() {
+  lcd.begin(16, 2);
+  lcd.print("Tervetuloa!");
+  pinMode(A0,INPUT);
+  delay(3000);
+
+}
+
+void loop() {
+  DHT.read11(dht_dpin);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("T= ");
+  lcd.print(DHT.temperature);
+  lcd.print(" C");
+  lcd.setCursor(0,1);
+  lcd.print("AH= ");
+  lcd.print(DHT.humidity);
+  lcd.print(" %");
+  delay(2000);
+  
+
+}
